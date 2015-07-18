8a048769e1ba924ffbc61edda199c2ad981d28a5
Julia Chrusciel <juliamchrusciel@gmail.com>
2015-07-17 23:18:16 -0500

    Changes to var correct

--- duolingo_one.js
+++ duolingo_one.js
@@ -36,6 +36,45 @@ var student_answer = ["The god sleeps in the house"]; //incorrect
 var student_answer = ["The dog stteeps in the house"]; //correct with typo
 var student_answer = ["The dog sttteeps in the house"]; //incorrect
 ////////////////////////////////////////////////////////////////////////////
+// const isCorrect = Boolean(true) if (var blame == [0] || var blame == [0]);
+//
+// var correct = true;
+// while (blame == "None" || "Typo") {
+//   return true;
+//   else return false;
+// }
+
+// var correct = [
+//   {none_c: true,
+//   blame: ["None"],
+//   },
+//   {typo_c: true,
+//   blame: ["Typo"],
+//   },
+//   {missing_f: false,
+//   blame: ["Missing"],
+//   },
+//     {wrong_word_f: false,
+//   blame: ["Wrong_word"],
+//   },
+// ];
+
+// var correct = [
+//   if (var blame == blame[0] || var blame == blame[1]) {
+//     document.correct(true);
+//   } else {
+//     document.correct(false);
+//   }
+// ];
+
+// var correct {
+//   if (blame[0] || blame[1]) {
+//     document.correct(true);
+//   } else {
+//     document.correct(false);
+//   }
+// };
+
 var correct = [true, false];
 var blame = ["None", "Typo", "Missing", "Wrong_word"];
 var highlights = ["[]"];
@@ -51,17 +90,9 @@ function grade(correct_answer, student_answer){
   }  else if (correct_answer.toLowerCase == student_answer.toLowerCase) {
   return (correct[0], blame[0], highlights[0]);
 
-  ////////////////////////////////////////////////
-  //Typo
-  ///////
-
   //Condition 4: Near Match because of Repeated Letter at any point in the sentence - Typo
   } else if student_answer correct_answer  {
   return (correct[0], blame[1], highlights[X]);
-
-  ////////////////////////////////////////////////
-  //Incorrect Answers
-  ///////
 
   //Condition 5: No Match because of omitted letter - False
 } else if (correct_answer.split(" ") != student_answer.split(" ")) ){
