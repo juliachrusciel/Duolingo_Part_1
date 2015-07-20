a1d2032e7b5c4e4c716471af4daf4d8de5c300d1
Julia Chrusciel <juliamchrusciel@gmail.com>
2015-07-20 00:51:35 -0500

    Adds conditions 5-7

--- duolingo_one.js
+++ duolingo_one.js
@@ -90,39 +90,85 @@ var student_answer = ["The dog sttteeps in the house"]; //incorrect
 // var blame = ["None", "Typo", "Missing", "Wrong_word"];
 // var highlights = ["[]"];
 
-function grade(correct_answer, student_answer){
-  var correct = [true, false];
-  var blame = ["none", "typo", "missing", "wrong_word"];
-  var highlights;
-  //For Condition 4-9:
-  var previous;
-  var boolWordDone = 0;
-  var correct_answer_string = [];
-  var student_answer_string = [];
-  var correct_answer_place = [];
-  var student_answer_place = [];
-  correct_answer_string = correct_answer.split(" ");
-  student_answer_string = student_answer.split(" ");
+  function grade(correct_answer, student_answer){
+    //for returned results
+    var correct = [true, false];
+    var blame = ["none", "typo", "missing", "wrong_word"];
+    var highlights;
+    //For Condition 2:
+    var punctuation = /[\u2000-\u206F\u2E00-\u2E7F\\'!"#\$%&\(\)\*\+,\-\.\/:;<=>\?@\[\]\^_`\{\|\}~]/g;
+    var space = /\s+/g;
+    var student_answer_no_punct= student_answer.replace(punctuation, '').replace(space, ' ');
+    //For Condition 4-7:
+    var previous;
+    var boolWordDone = 0;
+    var i, j, k, n; //loops and indices
 
-  //Condition 1: Exact Match - Correct
-  if (correct_answer == student_answer) {
-    highlights = "[]";
-    return "(" + correct[0] + ", " + blame[0] + ", " + highlights + ")";
+    var correct_answer_string = [];
+    var student_answer_string = [];
+    var correct_answer_place = [];
+    var student_answer_place = [];
+    correct_answer_string = correct_answer.split(" ");
+    student_answer_string = student_answer.split(" ");
+    //
 
-  //Condition 2: Exact Match but with Punctuation - Correct
-  } else if (correct_answer + "." || "!" || "?" || "," || ";" || ":" == student_answer + "." || "!" || "?" || "," || ";" || ":"){
-    highlights = "[]";
-    return "(" + correct[0] + ", " + blame[0] + ", " + highlights + ")";
+    //Condition 1: Exact Match - Correct
+    if (correct_answer == student_answer) {
+      highlights = "[]";
+      console.log("Correct Answer: \""+correct_answer + "\",  Student Answer: \""+student_answer + "\"");
+      console.log("Condition 1 check has ended.");
+      return "(" + correct[0] + ", " + blame[0] + ", " + highlights + ")";
 
-  //Condition 3: Exact Match but with different capitalization - Correct
-  }  else if (correct_answer.toLowerCase == student_answer.toLowerCase) {
-    highlights = "[]";
-    return "(" + correct[0] + ", " + blame[0] + ", " + highlights + ")";
+    //Condition 2: Exact Match but with Punctuation - Correct
+    } else if (correct_answer == student_answer_no_punct) {
+     highlights = "[]";
+     console.log("Correct Answer: \""+correct_answer + "\",  Student Answer: \""+student_answer + "\"");
+     console.log("Condition 2 check has ended.");
+     return "(" + correct[0] + ", " + blame[0] + ", " + highlights + ")";
 
-  //Condition 4: Near Match because of Repeated Letter at any point in the sentence - Typo
- } for (i = 0; i < correct_answer_string.length; i++ ); {
-    console.log("Correct Answer: \"" + correct_answer + "\",  Student Answer: \" " + student_answer + "\"");
+    //Condition 3: Exact Match but with different capitalization - Correct
+    }  else if ((correct_answer.toLowerCase()) == (student_answer.toLowerCase())) {
+      highlights = "[]";
+      console.log("Correct Answer: \""+correct_answer + "\",  Student Answer: \""+student_answer + "\"");
+      console.log("Condition 3 check has ended.");
+      return "(" + correct[0] + ", " + blame[0] + ", " + highlights + ")";
 
+
+    //Condition 4: Near Match because of Repeated Letter at any point in the sentence - Typo
+   } else if (correct_answer !== student_answer){
+      for (i = 0; i < correct_answer_string.length; i++);
+      console.log("Correct Answer: \""+correct_answer + "\",  Student Answer: \""+student_answer + "\"");
+
+      //Position Array for Correct Answer String and Debugging
+      for (var i = 0; i < correct_answer_string.length; i++) {
+        correct_answer_place[i] = correct_answer.search(correct_answer_string[i]);
+        console.log(correct_answer_string[i] + ", " + correct_answer_place[i]);
+      //Position Array for Student Answer String
+        } for (i = 0; i < student_answer_string.length; i++) {
+          student_answer_place[i] = student_answer.search(student_answer_string[i]);
+          console.log(student_answer_string[i] + ", " + student_answer_place[i]);
+        } for (var i = 0; i < correct_answer_string.length; i++){
+          for (var k = 0, j = 0; k < correct_answer_string[i].length && boolWordDone === 0; k++) {
+          console.log(correct_answer_string[i][k] + " " + student_answer_string[i][j]);
+      //If equal
+        if (correct_answer_string[i][k] == student_answer_string[i][j]) {
+          console.log("1");
+          previous = correct_answer_string[i][k];
+          continue;
+
+      //If Not equal
+      } else if (student_answer_string[i][j + 1] == previous) {
+        console.log("2");
+
+        outcome = "(" + correct_answer_place[i] + "," + correct_answer_string[i].length +")), (" + student_answer_place[i] + "," + student_answer_string[i].length + ")";
+        highlights = " [" + outcome + "]";
+        previous = " ";
+        console.log("Condition 4 check has ended.");
+        return "(" + correct[0] + ", " + blame[1] + ", " + highlights + ")";
+
+
+  //Condition 5: No Match because of omitted letter - False
+  } else if (correct_answer.split(" ") != student_answer.split(" ")) {
     //Position Array for Correct Answer String and Debugging
     for (var i = 0; i < correct_answer_string.length; i++) {
       correct_answer_place[i] = correct_answer.search(correct_answer_string[i]);
@@ -141,27 +187,135 @@ function grade(correct_answer, student_answer){
         continue;
       }
     //If Not equal
-      else if (student_answer_string[i][j + 1] == previous) {
+      else if (student_answer_string[i][j - 1] == previous) {
       console.log("2");
-
-      outcome = "(" + correct_answer_place[i] + "," + correct_answer_string[i].length +")), (" + student_answer_place[i] + "," + student_answer_string[i].length + ")";
-      highlights = " [" + outcome + "]";
-      previous = " ";
       }
-      console.log("Done.");
-      return "(" + correct[0] + ", " + blame[1] + ", " + highlights + ")";
-
+    outcome = "(" + correct_answer_place[i] + "," + correct_answer_string[i].length +")), (" + student_answer_place[i] + "," + student_answer_string[i].length + ")";
+    highlights = " [" + outcome + "]";
+    previous = " ";
+      console.log("Condition 5 check has ended.");
+      return "(" + correct[1] + ", " + blame[2] + ", " + highlights + ")";
       }
     }
   }
 
-  //Condition 5: No Match because of omitted letter - False
-  } else if (correct_answer.split(" ") != student_answer.split(" ")) {
+  //Condition 5: No Match because of missing letter - False
+  } else if (correct_answer.split(" ").length != student_answer.split(" ").length) {
+    //Position Array for Correct Answer String and Debugging
+    for (var i = 0; i < correct_answer_string.length; i++) {
+      correct_answer_place[i] = correct_answer.search(correct_answer_string[i]);
+      console.log(correct_answer_string[i] + ", " + correct_answer_place[i]);
+    //Position Array for Student Answer String
+      } for (i = 0; i < student_answer_string.length; i++) {
+        student_answer_place[i] = student_answer.search(student_answer_string[i]);
+        console.log(student_answer_string[i] + ", " + student_answer_place[i]);
+      } for (var i = 0; i < correct_answer_string.length; i++){
+        for (var k = 0, j = 0; k < correct_answer_string[i].length && boolWordDone === 0; k++) {
+        console.log(correct_answer_string[i][k] + " " + student_answer_string[i][j]);
+    //If equal
+      if (correct_answer_string[i][k] == student_answer_string[i][j]) {
+        console.log("1");
+        previous = correct_answer_string[i][k];
+        continue;
+      }
+    //If Not equal
+      else if (student_answer_string[i][j - 1] == previous) {
+      console.log("2");
+      }
+    outcome = "(" + correct_answer_place[i] + "," + correct_answer_string[i].length +")), (" + student_answer_place[i] + "," + student_answer_string[i].length + ")";
+    highlights = " [" + outcome + "]";
+    previous = " ";
+      console.log("Condition 5 check has ended.");
+      return "(" + correct[1] + ", " + blame[2] + ", " + highlights + ")";
+      }
+    }
+    //Condition 6: No Match because of wrong word - False
+    } else if (correct_answer.split(" ") != student_answer.split(" ")) {
+      //Position Array for Correct Answer String and Debugging
+      for (var i = 0; i < correct_answer_string.length; i++) {
+        correct_answer_place[i] = correct_answer.search(correct_answer_string[i]);
+        console.log(correct_answer_string[i] + ", " + correct_answer_place[i]);
+      //Position Array for Student Answer String
+        } for (i = 0; i < student_answer_string.length; i++) {
+          student_answer_place[i] = student_answer.search(student_answer_string[i]);
+          console.log(student_answer_string[i] + ", " + student_answer_place[i]);
+        } for (var i = 0; i < correct_answer_string.length; i++){
+          for (var k = 0, j = 0; k < correct_answer_string[i].length && boolWordDone === 0; k++) {
+          console.log(correct_answer_string[i][k] + " " + student_answer_string[i][j]);
+      //If equal
+        if (correct_answer_string[i][k] == student_answer_string[i][j]) {
+          console.log("1");
+          previous = correct_answer_string[i][k];
+          continue;
+        }
+      //If Not equal
+        else if (student_answer_string[i][j + 1] == previous) {
+        console.log("2");
+        }
+      outcome = "(" + correct_answer_place[i] + "," + correct_answer_string[i].length +")), (" + student_answer_place[i] + "," + student_answer_string[i].length + ")";
+      highlights = " [" + outcome + "]";
+      previous = " ";
+        console.log("Condition 6 check has ended.");
+        return "(" + correct[1] + ", " + blame[3] + ", " + highlights + ")";
+        }
+      }
+    }
+
+    //Condition 7: No Match because of missing word - False
+    } else (correct_answer.split(" ").length != student_answer.split(" ").length)  {
+        //Position Array for Correct Answer String and Debugging
+        for (var i = 0; i < correct_answer_string.length; i++) {
+          correct_answer_place[i] = correct_answer.search(correct_answer_string[i]);
+          console.log(correct_answer_string[i] + ", " + correct_answer_place[i]);
+        //Position Array for Student Answer String
+          } for (i = 0; i < student_answer_string.length; i++) {
+            student_answer_place[i] = student_answer.search(student_answer_string[i]);
+            console.log(student_answer_string[i] + ", " + student_answer_place[i]);
+          } for (var i = 0; i < correct_answer_string.length; i++){
+            for (var k = 0, j = 0; k < correct_answer_string[i].length && boolWordDone === 0; k++) {
+            console.log(correct_answer_string[i][k] + " " + student_answer_string[i][j]);
+        //If equal
+          if (correct_answer_string[i][k] == student_answer_string[i][j]) {
+            console.log("1");
+            previous = correct_answer_string[i][k];
+            continue;
+          }
+        //If Not equal
+          else if (student_answer_string[i][j - 1] == previous) {
+          console.log("2");
+          }
+        outcome = "(" + correct_answer_place[i] + "," + correct_answer_string[i].length +")), (" + student_answer_place[i] + "," + student_answer_string[i].length + ")";
+        highlights = " [" + outcome + "]";
+        previous = " ";
+          console.log("Condition 7 check has ended.");
+          return "(" + correct[1] + ", " + blame[2] + ", " + highlights + ")";
+          }
+        }
+    }
+
+
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     highlights = ;
     return "(" + correct[1] + ", " + blame[2] + ", " + highlights + ")";
 
-  //Condition 6: No Match because of wrong word - False
   } else if (correct_answer.split(" ").search !== student_answer.split(" ").search.match(correct_answer.split(" "))) {
+
     for (i = 0; i < correct_answer.length; i++);
     highlight = ;
     return "(" + correct[1] + ", " + blame[3] + ", " + highlights + ")";
@@ -173,3 +327,81 @@ function grade(correct_answer, student_answer){
     return "(" + correct[1] + ", " + blame[2] + ", " + highlights + ")";
   }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//     } else if (student_answer_string[n][j] == previous) {
+//       console.log("2A");
+//       console.log(j + " " + student_answer_string[n].length + " " + student_answer_string[n][j] + " " student_answer_string[n][j + 1]);
+//       if (j + 1 <= student_answer_string[n].length + " " + student_answer_string[n][j + 1]) { // continue to next one
+//         console.log("2B");
+//         blame_b = blame[1];
+//         outcome = "((" + correct_answer_place[i] + ", " + (correct_answer_place[i] + correct_answer_string[i].length) + ")), (" + student_answer_string[n] + "," + (student_answer_place[n] + student_answer_string[n]) + "))";
+//         previous = " ";
+//         j = j + 2;
+//         if (j >= student_answer_string[n].length) {
+//           console.log("2C");
+//           blame_b = blame[3];
+//           boolWordDone = 1;
+//         }
+//         boolWordDone = 1;
+//     } else { //next one match fails so continue
+//       console.log("3");
+//       blame_b = blame[3];
+//       boolWordDone = 1;
+//       outcome = "((" + correct_answer_place[i] + ", " + (correct_answer_place[i] + correct_answer_string[i].length) + ")), (" + student_answer_string[n] + "," + (student_answer_place[n] + student_answer_string[n]) + "))";
+//       previous = " ";
+//     }
+//       //If missing - Condition 5: No Match because of omitted letter - False
+//     } else if (k === 0) {
+//       console.log("4A i + 1-" + i + " correct_answer_string[i + 1]" + correct_answer_string[i + 1]+ ". n-" + n + "student_answer_string[n][j]" + student_answer_string[n][j]);
+//       //is there another word after this one?
+//         if (i + 1 < correct_answer_string.length && correct_answer_string[i + 1][0] == student_answer_string[n][j] || i + 2 < correct_answer_string && correct_answer_string[i + 2][0] == student_answer_string[n][j] ){
+//           wantNextSAS = 0;
+//           blame_b = blame[3];
+//           boolWordDone = 1;
+//           outcome = "((" + correct_answer_place[i] + ", " + (correct_answer_place[i] + correct_answer_string[i].length) + "))" + ",(" + student_answer_place[n] + "," + student_answer_place[n] + "))";
+//           console.log("4B " + outcome);
+//
+//         } else if (i + 2 < correct_answer_string.length && correct_answer_string[i + 2][0] == student_answer_string[n][j]) {
+//           //second is missing so Done
+//           outcome = "";
+//           result = correct[1];
+//           blame_b = blame[2];
+//           console.log("4C " + outcome);
+//           boolWordDone = 1;
+//           bGiveUp = 1;
+//         }
+//       } else {
+//         console.log("4D");
+//         blame_b = blame[0];
+//         break;
+//       }
+//     }
+//     console.log("6 " + outcome)
+//     if (j < student_answer_string[n].length && blame_b != blame[1] || blame_b != blame[2]){
+//       console.log("5 j" + j  + " i" + i + ", n" + n);
+//       blame_b = blame[3];
+//       outcome = "((" + correct_answer_place[i] + ", " + (correct_answer_place[i] + correct_answer_string[i].length) + ")), (" + student_answer_string[n] + "," + (student_answer_place[n] + student_answer_string[n]) + "))";
+//     }
+//
+//   }
+//   if(blame_b == blame[3] || blame_b = blame[2]){
+//     result = correct[1];
+//     highlights = "[" + outcome + "]";
+//     return "(" + correct[1] + ", " + blame_b + ", " + highlights + ")";
+//   }
+// }
